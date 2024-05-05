45b95608541775da7f1513af78033e71e3ff1617
David Cote <dmc1991@icloud.com>
2024-05-05 09:38:04 -0400

    Added constructor methods to Game and Phrase classes

--- js/Game.js
+++ js/Game.js
@@ -3,5 +3,15 @@
  * Game.js */
 
 class Game {
-    
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase(`She sells seashells by the seashore`),
+            new Phrase(`All is well that ends well`),
+            new Phrase(`To infinity and beyond`),
+            new Phrase(`Actions speak louder than words`),
+            new Phrase(`Contentment makes poor men rich`)
+        ];
+        this.activePhrase = null;
+    }
 }

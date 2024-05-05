9be7f8cb590f650f04db657605c23d1395ebef90
David Cote <dmc1991@icloud.com>
2024-05-05 09:47:09 -0400

    Added click listener to btnReset

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const btnReset = document.getElementById(`btn__reset`);
+let game;
+
+btnReset.addEventListener(`click`, () => {
+    game = new Game();
+    game.startGame();
+});

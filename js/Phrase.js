846263ba0f872c8ad24ebbc42eaed14d8f2964a2
David Cote <dmc1991@icloud.com>
2024-05-05 09:42:53 -0400

    Added addPhraseToDisplay() method to Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,21 @@ class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }
+    /**
+    * Displays phrase on game board
+    */
+    addPhraseToDisplay(){
+        const phraseUl = document.querySelector(`#phrase ul`);
+        const phraseChars = this.phrase.split(``);
+        phraseChars.forEach(char => {
+            const li = document.createElement(`li`);
+            if (char !== ` `){
+                li.className = `hide letter ${char}`;
+                li.textContent = char;
+            } else {
+                li.className = `space`;
+            }
+            phraseUl.appendChild(li);
+        });
+    }
 }

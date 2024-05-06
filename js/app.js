fc6e52fba0eb6f22e49c0d72e84ef5fbfdfeabdf
David Cote <dmc1991@icloud.com>
2024-05-06 13:06:52 -0400

    Added gameboard reset functionality to brnReset click listener

--- js/app.js
+++ js/app.js
@@ -5,11 +5,29 @@
 const btnReset = document.getElementById(`btn__reset`);
 let game;
 
+/**
+ * Resets the gameboard between games
+ */
 btnReset.addEventListener(`click`, () => {
+    const phraseUl = document.querySelector('#phrase ul');
+    phraseUl.innerHTML = ``;
+    const keyboardButtons = document.querySelectorAll('.key');
+    keyboardButtons.forEach(button => {
+        button.disabled = false;
+        button.classList.remove('chosen', 'wrong');
+        button.classList.add('key'); 
+    });
+    const hearts = document.querySelectorAll('.tries img');
+    hearts.forEach(heart => {
+        heart.src = 'images/liveHeart.png';
+    });
     game = new Game();
     game.startGame();
 });
 
+/**
+ * Gives functionality to the keyboard
+ */
 const keyboard = document.getElementById('qwerty');
 keyboard.addEventListener('click', (e) => {
     if (e.target.classList.contains('key')) {
